e2692d1e13decc55edfd5c6dee1cc4a9bd28e65c
taekop <seungtaek@hyperithm.com>
2021-12-29 16:04:58 +0900

    Implement fetching IERC721 event

--- scripts/web3.ts
+++ scripts/web3.ts
@@ -1,41 +1,53 @@
 import "@nomiclabs/hardhat-web3";
+import { AbiCoder } from "ethers/lib/utils";
 import { ethers, web3 } from "hardhat";
 
 async function main() {
-    // some nft address
-    const address = "0xdef6e293930b5598cbf1c1a8cb1b0cbd0deb5708";
+    /***** some nft address *****/
+    const address = "0x22b99ff14b888739547f8f38548a866cc8b3a1bc";
 
-    const code = await ethers.provider.getCode(address);
-    console.log(code);
-
-    const names = ['name', 'totalSupply', 'paused', 'symbol', 'owner'];
-    for (const name of names) {
-        let res = await web3.eth.call({
-            to: address,
-            data: web3.eth.abi.encodeFunctionCall(
-                {
-                    type: "function",
-                    name: name,
-                    inputs: [],
-                },
-                []
-            ),
-        });
-        console.log(name + ' ' + res);
+    /***** get past events *****/
+    const contract = await ethers.getContractAt('IERC721', address);
+    const eventFilter = contract.filters.Transfer('0x0000000000000000000000000000000000000000');
+    const logs = await contract.queryFilter(eventFilter, 78700000, 'latest');
+    for (const log of logs) {
+        let tokenId = <any>(<any>log.args).tokenId.toNumber();
     }
+}
 
-    let res = await web3.eth.call({
-        to: address,
-        data: web3.eth.abi.encodeFunctionCall(
-            {
-                type: "function",
-                name: 'tokenURI',
-                inputs: [{ type: 'uint256', name: '' }],
-            },
-            ['1']
-        ),
-    });
-    console.log(res);
+
+    /***** get information *****/
+    // const code = await ethers.provider.getCode(address);
+    // console.log(code);
+
+    // const names = ['baseTokenURI'];['name', 'totalSupply', 'paused', 'symbol', 'owner'];
+    // for (const name of names) {
+    //     let res = await web3.eth.call({
+    //         to: address,
+    //         data: web3.eth.abi.encodeFunctionCall(
+    //             {
+    //                 type: "function",
+    //                 name: name,
+    //                 inputs: [],
+    //             },
+    //             []
+    //         ),
+    //     });
+    //     console.log(name + ' ' + res);
+    // }
+
+    // let res = await web3.eth.call({
+    //     to: address,
+    //     data: web3.eth.abi.encodeFunctionCall(
+    //         {
+    //             type: "function",
+    //             name: 'tokenURI',
+    //             inputs: [{ type: 'uint256', name: '' }],
+    //         },
+    //         ['123']
+    //     ),
+    // });
+    // console.log(res);
 };
 
 
